56af7177f69eaacd423d98dbf331225c3fbc74ce
Evgeny Saltykov <es@zanami.ru>
2023-04-03 13:20:02 +1000

    trying to get rid of default channel

--- packages/checkout-storefront/src/lib/regions.ts
+++ packages/checkout-storefront/src/lib/regions.ts
@@ -1,7 +1,7 @@
-export const locales = ["en-US", "pl-PL"] as const;
+export const locales = ["en-US"] as const;
 
 export const DEFAULT_LOCALE = "en-US";
 
-export const DEFAULT_CHANNEL = "default-channel";
+export const DEFAULT_CHANNEL = "ug";
 
 export type Locale = typeof locales[number];
