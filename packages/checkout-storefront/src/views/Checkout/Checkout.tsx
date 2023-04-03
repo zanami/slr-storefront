10d456dfe697c33a4744a6dd925d416bbafab5c8
Evgeny Saltykov <es@zanami.ru>
2023-04-03 17:40:04 +1000

    debugcheckout

--- packages/checkout-storefront/src/views/Checkout/Checkout.tsx
+++ packages/checkout-storefront/src/views/Checkout/Checkout.tsx
@@ -19,30 +19,32 @@ export const Checkout = () => {
 
   const isEmptyCart = checkout && !checkout.lines.length;
 
+  console.log("Checkout / isCheckoutInvalid", checkout, isCheckoutInvalid);
+
   return isCheckoutInvalid ? (
     <PageNotFound />
   ) : isInitiallyAuthenticating ? (
     <CheckoutSkeleton />
   ) : (
-    // <ErrorBoundary FallbackComponent={PageNotFound}>
-    <div className="page">
-      <PageHeader />
-      <div className="page-content">
-        {isEmptyCart ? (
-          <EmptyCartPage />
-        ) : (
-          <>
-            <Suspense fallback={<CheckoutFormSkeleton />}>
-              <CheckoutForm />
-            </Suspense>
-            <div className="page-divider" />
-            <Suspense fallback={<SummarySkeleton />}>
-              <Summary {...checkout} />
-            </Suspense>
-          </>
-        )}
+    <ErrorBoundary FallbackComponent={PageNotFound}>
+      <div className="page">
+        <PageHeader />
+        <div className="page-content">
+          {isEmptyCart ? (
+            <EmptyCartPage />
+          ) : (
+            <>
+              <Suspense fallback={<CheckoutFormSkeleton />}>
+                <CheckoutForm />
+              </Suspense>
+              <div className="page-divider" />
+              <Suspense fallback={<SummarySkeleton />}>
+                <Summary {...checkout} />
+              </Suspense>
+            </>
+          )}
+        </div>
       </div>
-    </div>
-    // </ErrorBoundary>
+    </ErrorBoundary>
   );
 };
